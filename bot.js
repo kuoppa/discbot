5c62afae92035ddb4b79a95afaeaf74a78da482e
Martin Kuoppa <kuoppa.martin@gmail.com>
2016-12-27 19:18:15 +0100

    added help command

--- bot.js
+++ bot.js
@@ -4,13 +4,29 @@ var bot = new Discord.Client();
 const setting = require("./src/disc-token.json");
 const ddif = require('return-deep-diff');
 
+const commands = {
+  "help" : "Shows available commands",
+  "team" : "Shows current members in the Super Mega Team in dota 2",
+  "yee"  : "You know.."
+};
+
 bot.on("message", msg => {
   prefix = "!";
+  if(msg.author.bot) return;
   if (msg.content.startsWith(prefix+"yee")) {
     msg.channel.sendMessage("<:yee:245162008020779008>  ");
   }
+
   if(msg.content.startsWith('@Dominatrix')){
     msg.channel.sendMessage("Did I tell you that you could speak to me?");
+  }
+
+  if (msg.content.startsWith(prefix+"help")) {
+    var helpMessage = ""
+    for(var c in commands){
+      helpMessage += "**"+prefix+c+"**    : "+commands[c]+"\n";
+    }
+    msg.channel.sendMessage(helpMessage);
   }
 
   if (msg.content.startsWith("!team")) {
