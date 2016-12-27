06fa59bb279441c6c53ed669453f374a4b3c8978
Martin Kuoppa <kuoppa.martin@gmail.com>
2016-12-27 20:14:12 +0100

    fixat till så att botten svarar lite

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 
-const setting = require("./src/disc-token.json");
+const setting = require("./src/disc.json");
 const ddif = require('return-deep-diff');
 
 const commands = {
   "help" : "Shows available commands",
-  "team" : "Shows current members in the Super Mega Team in dota 2",
-  "yee"  : "You know.."
+  "team" : "Shows current members of the SuperMegaTeam in dota 2",
+  "yee"  : "You know..",
+  "ping" : "Pings the members of Super Mega Team"
 };
 
 bot.on("message", msg => {
@@ -17,14 +18,24 @@ bot.on("message", msg => {
     msg.channel.sendMessage("<:yee:245162008020779008>  ");
   }
 
-  if(msg.content.startsWith('@Dominatrix')){
-    msg.channel.sendMessage("Did I tell you that you could speak to me?");
+
+  if(msg.content.startsWith('<@263313776554672128>')){
+    msg.channel.sendMessage("Did I tell you that you were allowed to talk to me? taste my whip!");
   }
+
+  if (msg.content.startsWith(prefix+"ping")) {
+    for(var m in msg.member._roles){
+      if(msg.member._roles[m] !== setting.smtid){
+        msg.channel.sendMessage("@SuperMegaTeam");
+      }
+    }
+  }
+
 
   if (msg.content.startsWith(prefix+"help")) {
     var helpMessage = ""
     for(var c in commands){
-      helpMessage += "**"+prefix+c+"**    : "+commands[c]+"\n";
+      helpMessage += "**"+prefix+c+"**\n"+commands[c]+"\n\n";
     }
     msg.channel.sendMessage(helpMessage);
   }
@@ -39,6 +50,12 @@ bot.on("message", msg => {
     });
   }
 });
+
+
+
+
+
+
 
 bot.on('guildCreate', guild => {
   guild.defaultChannel.sendMessage('-- Obey me, or I will ban you with my whip! --');
